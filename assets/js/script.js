dea191e1959310ca6e0bd0e631528b6259878bc1
Anthony O'Brien <aux.fuse@yahoo.com>
2021-11-13 22:36:05 +0000

    JS added to detect browser width and update field-of-view attribute on model-viewer elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,19 @@
+
+
+/**
+ * Detect browser width to update the field of view of both Model-viewer elements
+ * */
+
+let innerWidth = window.innerWidth;
+const modelViewer = document.querySelectorAll('model-viewer');
+console.log(modelViewer);
+
+if (innerWidth < "992") {
+    for (let el of modelViewer) {
+        el.setAttribute('field-of-view', '45deg');
+    }
+};
+
 // -------------------------------------------------------------------- Copyright
 
 function copyrightYear() {
