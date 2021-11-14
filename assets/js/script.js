61b1b7b0f02405fa9c333df91f58b86a376d80ff
Abibubble <abi.yamaha.neo@gmail.com>
2021-11-14 20:33:08 +0000

    #13 ref: Refactor all code, ensure all JS works, grammar edits

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,30 +40,3 @@ if (innerWidth < "992") {
         el.setAttribute('field-of-view', '45deg');
     }
 };
-
-// -------------------------------------------------------------------- Navbar 
-
-let burgerIcon = document.getElementById("nav-burger");
-let closeIcon = document.getElementById("close-icon");
-let navbar = document.getElementById("menu");
-
-burgerIcon.addEventListener("click", toggleNavbar);
-closeIcon.addEventListener("click", toggleNavbar);
-
-function toggleNavbar() {
-    if (navbar.classList.contains("navbar-toggle")) {
-        navbar.classList.remove("navbar-toggle");
-    } else {
-        navbar.classList.add("navbar-toggle");
-    }
-}
-
-// -------------------------------------------------------------------- Copyright
-
-function copyrightYear() {
-    var d = new Date();
-    var y = d.getFullYear();
-    document.getElementById("copyright").innerHTML = y;
-}
-
-copyrightYear();
