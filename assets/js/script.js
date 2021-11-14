f16a1bde35dccd2ad31ae82daaaa9238eed213f2
Anthony O'Brien <aux.fuse@yahoo.com>
2021-11-14 16:59:03 +0000

    JS added to handle on click events of rotate & interactive buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,37 @@
+// -------------------------------------------------------------------- Globals
+
+const interactiveButton = document.querySelector('#interactive-controls');
+const rotateButton = document.querySelector('#auto-rotate');
+let innerWidth = window.innerWidth;
+const modelViewer = document.querySelectorAll('model-viewer');
+
+// -------------------------------------------------------------------- Interactive
+
+interactiveButton.addEventListener("click", () => {
+    for (let el of modelViewer) {
+        el.setAttribute('camera-controls', '');
+        el.style.pointerEvents = 'auto';
+    }
+});
+
+rotateButton.addEventListener("click", () => {
+    for (el of modelViewer) {
+        el.removeAttribute('auto-rotate');
+    }
+});
+
+// -------------------------------------------------------------------- Awareness
+
 /**
  * Detect browser width to update the field of view of both Model-viewer elements
  * */
-
-let innerWidth = window.innerWidth;
-const modelViewer = document.querySelectorAll('model-viewer');
-console.log(modelViewer);
 
 if (innerWidth < "992") {
     for (let el of modelViewer) {
         el.setAttribute('field-of-view', '45deg');
     }
 };
+
 // -------------------------------------------------------------------- Header 
 
 function openNavBar() {
